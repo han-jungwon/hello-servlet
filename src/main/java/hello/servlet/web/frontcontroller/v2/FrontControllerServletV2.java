2bf3d3b5d01fc2c103c25141e102875f6f42e294
wonny2 <rhehf1024@gmail.com>
2022-12-26 15:47:39 +0900

    단순하고 실용적인 컨트롤러 - v4

--- src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -17,12 +17,12 @@ import java.util.Map;
 @WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*")
 public class FrontControllerServletV2 extends HttpServlet {
 
-    private Map<String, ControllerV2> controllerV2Map = new HashMap<>();
+    private Map<String, ControllerV2> controllerMap = new HashMap<>();
 
     public FrontControllerServletV2() {
-        controllerV2Map.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
-        controllerV2Map.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
-        controllerV2Map.put("/front-controller/v2/members", new MemberListControllerV2());
+        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerMap.put("/front-controller/v2/members", new MemberListControllerV2());
     }
 
 
@@ -30,7 +30,7 @@ public class FrontControllerServletV2 extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String requestURI = request.getRequestURI();
 
-        ControllerV2 controller = controllerV2Map.get(requestURI);
+        ControllerV2 controller = controllerMap.get(requestURI);
         if (controller == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
